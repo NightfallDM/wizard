efa29869532274ace1e6a0c1fe79732f92e6c089
night <night@halfware.top>
2021-11-28 12:51:10 +0800

    add fn 'put'

--- datastructure/tree/bt/rbtree.rs
+++ datastructure/tree/bt/rbtree.rs
@@ -1,7 +1,7 @@
 use std::ptr::NonNull;
 use std::fmt::Display;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 enum Color {
     Red,
     Black,
@@ -38,6 +38,15 @@ impl<K: PartialOrd, V: Display> RbNode<K, V> {
 
     fn nn_to_mut<'a>(mut node_nnptr: NonNull<Self>) -> &'a mut Self {
         unsafe{node_nnptr.as_mut()}
+    }
+
+    fn is_red(node_nnptr_opt: Option<NonNull<Self>>) -> bool {
+        match node_nnptr_opt {
+            None => false,
+            Some(node_nnptr) => {
+                RbNode::nn_to_ref(node_nnptr).color == Color::Red
+            },
+        }
     }
 }
 
@@ -94,6 +103,41 @@ impl<K: PartialOrd, V: Display> RbTree<K, V> {
         curr_node.color = Color::Red;
         RbNode::nn_to_mut(curr_node.left.unwrap()).color = Color::Black;
         RbNode::nn_to_mut(curr_node.right.unwrap()).color = Color::Black;
+    }
+
+    fn _put(node_nnptr_opt: Option<NonNull<RbNode<K, V>>>, key: K, value: V) -> NonNull<RbNode<K, V>> {
+        if let None = node_nnptr_opt {
+            return unsafe{NonNull::new_unchecked(RbNode::new_box_leak(key, value, Color::Red) as *mut _)};
+        }
+
+        let mut curr = RbNode::nn_to_mut(node_nnptr_opt.unwrap());
+        if curr.key > key {
+            curr.left = Some(RbTree::_put(curr.left, key, value));
+        }else if curr.key < key {
+            curr.right = Some(RbTree::_put(curr.right, key, value));
+        }else {
+            curr.key = key;
+            curr.value = value;
+        }
+        
+        if RbNode::is_red(curr.right) && !RbNode::is_red(curr.left){
+            curr = RbNode::nn_to_mut(RbTree::_totate_left(unsafe{NonNull::new_unchecked(curr as *mut _)}));
+        }
+
+        if RbNode::is_red(curr.left) && RbNode::is_red(RbNode::nn_to_ref(curr.left.unwrap()).left) {
+            curr = RbNode::nn_to_mut(RbTree::_totate_right(curr.left.unwrap()));
+        }
+
+        if RbNode::is_red(curr.right) && RbNode::is_red(curr.left) {
+            RbTree::_flip_color(unsafe{NonNull::new_unchecked(curr as *mut _)});
+        }
+
+        unsafe{NonNull::new_unchecked(curr as *mut _)}
+    }
+
+    pub fn put(&mut self, key: K, value: V) {
+        self.root = Some(Self::_put(self.root, key, value));
+        RbNode::nn_to_mut(self.root.unwrap()).color = Color::Red;
     }
 }
 
