671b0ee14b6213fe5555ed009f6b8369250718b3
night <night@halfware.top>
2022-03-31 15:50:04 +0800

    some kernel phy to virt mem relative

--- kernel_work/date_mark/9-Feb-2022/simple_print.c
+++ kernel_work/date_mark/9-Feb-2022/simple_print.c
@@ -8,6 +8,7 @@ static void *vm_addr;
 int simple_prt_init(void) {
     vm_addr = vmalloc(4096);
     printk("0x%016lx", (unsigned long)vm_addr);
+    printk("y = 0x%016lx\n",(unsigned long)((unsigned long)vm_addr - 0xffffffff80000000UL));
     return 0;
 }
 
